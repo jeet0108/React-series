4acc4d6fadc3bf57503631a85c2e656cbb374d91
jeetpatel01 <132487600+jeetpatel01@users.noreply.github.com>
2024-02-06 02:33:56 +0530

    react

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () =>{
+    setCounter(prevCounter => prevCounter + 1)
+
+      // counter = counter + 1
+  }
+  const removeValue = () =>{
+    setCounter(prevCounter => prevCounter - 1 )
+
+      // counter = counter - 1
+  }
+
+  return (
+    <>
+     <h1>Jeet Sabhadiya</h1>
+     <h2>Counter Value :{counter}</h2>
+
+     <button
+     onClick={addValue}
+     >Add value</button>
+     <br />
+     <button
+     onClick={removeValue}
+     >Remove Value</button>
+
+     <p>footer : {counter}</p>
+    </>
+  )
+}
+
+export default App;
---
+++ vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  // eslint-disable-next-line no-unused-vars
+  const username = "jeet"
+
+  return (
+    <>
+    <h1>jeet sabhadiya {username}</h1>
+    <p>testing</p>
+    </>
+  )
+    
+     
+}
+
+export default App
