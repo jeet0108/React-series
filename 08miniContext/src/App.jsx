422fdae9da331cbd6d462897dcf85ab877ea9985
jeetpatel01 <132487600+jeetpatel01@users.noreply.github.com>
2024-03-05 13:40:22 +0530

    contextAPI

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import React from 'react'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+    <h1>Jeet Sabhadiya</h1>
+    <Login/>
+    <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
